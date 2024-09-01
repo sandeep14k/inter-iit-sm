923f723148edb6351137efef35ed46f571ab5ba6
saatvik1879 <saatvik1879@gmail.com>
2024-09-02 00:56:30 +0530

    search added

--- src/Components/Timeline.jsx
+++ src/Components/Timeline.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import '../css/Schedule.css';
 
-function Timeline() {
+const Timeline = ({ onDateSelect }) => {
+  const handleDateClick = (date) => {
+    onDateSelect(date);
+  };
+
   return (
     <div className="timeline">
       <div className="indicator">On</div>
       <div className="bar">
-      <button className="date">19th Dec</button>
-      <button className="date">19th Dec</button>
-      <button className="date">19th Dec</button>
-      <button className="date">19th Dec</button>
-      <button className="date">19th Dec</button>
-      <button className="date">19th Dec</button>
-      <button className="date">19th Dec</button>
+        <button className="date" onClick={() => handleDateClick('2024-12-18T00:00:00.000Z')}>18th Dec</button>
+        <button className="date" onClick={() => handleDateClick('2024-12-19T00:00:00.000Z')}>19th Dec</button>
+        <button className="date" onClick={() => handleDateClick('2024-12-20T00:00:00.000Z')}>20th Dec</button>
+        <button className="date" onClick={() => handleDateClick('2024-12-21T00:00:00.000Z')}>21st Dec</button>
+        <button className="date" onClick={() => handleDateClick('2024-12-22T00:00:00.000Z')}>22nd Dec</button>
+        <button className="date" onClick={() => handleDateClick('2024-12-23T00:00:00.000Z')}>23rd Dec</button>
+        <button className="date" onClick={() => handleDateClick('2024-12-24T00:00:00.000Z')}>24th Dec</button>
       </div>
     </div>
   );
-}
+};
 
 export default Timeline;
+
