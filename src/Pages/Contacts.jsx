04d733627fb77500fdbd1454f43719dc227a9f91
siddhu0721 <sidsiddhu7015@gmail.com>
2024-09-08 19:23:27 +0530

    modify contacts page

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Teams from '../Components/Teams'
-import Footer from '../Components/Footer'
 
 const Contacts = () => {
   return (
     <>
       <Navbar/>
       <Teams/>
-      <Footer/>
     </>
   )
 }
