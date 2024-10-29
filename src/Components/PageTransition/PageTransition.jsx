9f2cda70e07e32eeb7590ffffcd21bc98c6ae4e0
Vibhor Srivastava <153298754+itsmevibhor2005@users.noreply.github.com>
2024-10-29 18:37:42 +0530

    Add files via upload

---
+++ src/Components/PageTransition/PageTransition.jsx
@@ -0,0 +1,15 @@
+import { motion } from "framer-motion";
+const Transition = ({children}) =>{
+    return (
+        <motion.div
+            initial={{ opacity: 0, x:100 }}
+            animate={{ opacity: 1, x:0 }}
+            exit={{ opacity: 0, x:-100 }}
+            transition={{ duration: 0.5 }}  
+        >
+            {children}
+        </motion.div>
+    );
+}
+
+export default Transition;
