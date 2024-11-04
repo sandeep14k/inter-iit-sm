7823e0a0b95717bab43cbc9d49393be176be33ba
Ujjwal1152 <ujjawal22@iitk.ac.in>
2024-11-04 21:38:35 +0530

    Minor changes and reduandant files removed

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,39 +1,18 @@
 import React from 'react'
 import Photos from '../Components/Photos'
-import Navbar from '../Components/Navbar'
-import Footer from '../Components/Footer'
-import { Carousel } from "antd";
-
 import Transition from "../Components/PageTransition/PageTransition";
-// import "../css/Gallery.css";
 
 const Gallery = () => {
-  const contentStyle = {
-    height: '450px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-  };
+  // const contentStyle = {
+  //   height: '450px',
+  //   color: '#fff',
+  //   lineHeight: '160px',
+  //   textAlign: 'center',
+  //   background: '#364d79',
+  // };
   return (
       <Transition>
     <div>
-      {/* <div >
-        <Carousel autoplay effect="fade" >
-          <div style={contentStyle}>
-            <img src="/gallery/cricket3.jpg" alt=""/>
-          </div>
-          <div>
-            <img src="/gallery/chess1.jpg" alt="" />
-          </div>
-          <div>
-            <img src="/gallery/football2.JPG" alt="" />
-          </div>
-          <div>
-            <img src="/gallery/hockey1.jpg" alt="" />
-          </div>
-        </Carousel>
-      </div> */}
       <div className='photo-sep'>Gallery<div><hr className='custom-line'></hr></div></div>
       <Photos/>
     </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,8 @@
-
 import React from 'react'
-import Footer from '../Components/Footer'
 import Sponser from '../Components/Sponser'
 import P_IITs from '../Components/P_IITs'
 import Sports from '../Components/Sports'
 import About from '../Components/About'
-import Navbar from '../Components/Navbar'
 import Header from '../Components/Header'
 import SocialMediaHandles from '../Components/SocialMediaHandles'
 import FeaturedEvent from '../Components/featured_event'
