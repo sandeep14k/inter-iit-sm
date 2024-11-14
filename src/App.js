e7be78646e86cc3f4aecbaeaf301b259ec6db54f
Ujjwal1152 <ujjawal22@iitk.ac.in>
2024-11-14 12:07:40 +0530

    yarn instead of npm and meta description added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 import Home from "./Pages/Home";
 import Schedule from "./Pages/Schedule";
 import Result from "./Pages/Result";
@@ -12,71 +13,61 @@ import Footer from './Components/Footer';
 import './App.css';
 import { AnimatePresence } from 'framer-motion';
 import Loader from './Components/PageTransition/Loader';
-import axios from 'axios';
-import Selection from './Pages/Selection'; // New Selection page
+import Selection from './Pages/Selection';
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [role, setRole] = useState(localStorage.getItem('userRole') || null); // New state to store user role
+  const [role, setRole] = useState(localStorage.getItem('userRole') || null);
 
-  // Save role to local storage whenever it changes
   useEffect(() => {
     if (role) {
       localStorage.setItem('userRole', role);
     }
   }, [role]);
 
-  // Function to handle setting a new role
   const handleRoleChange = (newRole) => {
     setRole(newRole);
     localStorage.setItem('userRole', newRole);
   };
 
-  // Function to clear the role and redirect to selection page
   const handleClearRole = () => {
     setRole(null);
-    localStorage.removeItem('userRole'); // Remove role from local storage
+    localStorage.removeItem('userRole');
   };
 
   if (!role) {
     return <Selection setRole={handleRoleChange} />;
   }
 
+  const metaDescriptionContent = role === 'student'
+    ? "Welcome to the Inter IIT Sports Meet 2024! Stay updated with live scores, schedules, athlete highlights, and more from India’s premier intercollegiate sports event."
+    : "Explore the 29th Inter IIT Staff Sports Meet. Stay informed with event schedules, team highlights, and updates for IIT staff’s exciting annual competition.";
 
   return (
-    <BrowserRouter>
-      <Navbar role={role} /> {/* Pass role to Navbar for role-specific items */}
-      <Loader show={loading} />
-      <AnimatePresence>
-        <div className={role ? 'app-content' : ''}>
-        <Routes>
-          {/* Conditionally render routes based on selected role */}
-          {role === 'student' ? (
-            <>
-              <Route path="/" element={<Home role={role}/>} />
+    <HelmetProvider>
+      <BrowserRouter>
+        <Helmet>
+          <title>Inter IIT Sports Meet 2024</title>
+          <meta name="description" content={metaDescriptionContent} />
+        </Helmet>
+        <Navbar role={role} />
+        <Loader show={loading} />
+        <AnimatePresence>
+          <div className={role ? 'app-content' : ''}>
+            <Routes>
+              <Route path="/" element={<Home role={role} />} />
               <Route path="/Result" element={<Result role={role} />} />
-              <Route path="/Schedule" element={<Schedule role={role}/>} />
-              <Route path="/Livescores" element={<Livescores role={role}/>} />
-              <Route path="/Athletes" element={<Athletes role={role}/>} />
-              <Route path="/Contacts" element={<Contacts role={role} />} />
-              <Route path="/gallery" element={<Gallery role={role} />} />
-            </>
-          ) : (
-            <>
-              <Route path="/" element={<Home role={role}/>} />
-              <Route path="/Result" element={<Result role={role}/>} />
-              <Route path="/Schedule" element={<Schedule role={role}/>} />
+              <Route path="/Schedule" element={<Schedule role={role} />} />
               <Route path="/Livescores" element={<Livescores role={role} />} />
-              <Route path="/Athletes" element={<Athletes role={role}/>} />
-              <Route path="/Contacts" element={<Contacts role={role}/>} />
-              <Route path="/gallery" element={<Gallery role={role}/>} />
-            </>
-          )}
-        </Routes>
-       </div>
-      </AnimatePresence>
-      <Footer onRoleChange={handleClearRole} /> 
-    </BrowserRouter>
+              <Route path="/Athletes" element={<Athletes role={role} />} />
+              {role === 'student' && <Route path="/Contacts" element={<Contacts role={role} />} />}
+              <Route path="/gallery" element={<Gallery role={role} />} />
+            </Routes>
+          </div>
+        </AnimatePresence>
+        <Footer onRoleChange={handleClearRole} />
+      </BrowserRouter>
+    </HelmetProvider>
   );
 };
 
