b9b62f84be3394fdc121950a7a4e4d55019104e5
Prathamesh Thorat <thoratprathamesh1729@gmail.com>
2024-12-01 19:28:22 +0530

    location mapping added

--- src/Components/Schedule/MatchCardR.jsx
+++ src/Components/Schedule/MatchCardR.jsx
@@ -4,46 +4,70 @@ import "../../css/MatchCardR.css";
 import { SiGooglemaps, SiYoutube } from "react-icons/si";
 import { HiClock, HiCalendarDays } from "react-icons/hi2";
 import { SportName, TeamLogo } from "./utils/SportCardUtils";
-import { BasketScore, CricketScore, HockeyScore, LawnTennisScore } from "./utils/ScoreUI";
+import {
+  BasketScore,
+  CricketScore,
+  HockeyScore,
+  LawnTennisScore,
+} from "./utils/ScoreUI";
+
+let locationVenueMap = {
+  cricket: "https://maps.app.goo.gl/FpTCAMRcANB4gZPA6",
+  volleyball: "https://maps.app.goo.gl/N1Wifb1BHsvNWGR8A",
+  basketball: "https://maps.app.goo.gl/6sn6m9muKxZHhYoN7",
+  "lawn tennis": "https://maps.app.goo.gl/YLZz5zWsPFpyZ6wR8",
+  hockey: "https://maps.app.goo.gl/uHzKub8eMjU5ELnKA",
+  "table tennis": "https://maps.app.goo.gl/mS963wEW1xnDaKxb8",
+};
 
 function formatAMPM(date) {
   var hours = parseInt(date.split(":")[0]);
   var minutes = parseInt(date.split(":")[1]);
-  var ampm = hours >= 12 ? 'pm' : 'am';
+  var ampm = hours >= 12 ? "pm" : "am";
   hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + ampm;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
   return strTime;
 }
 
 function MatchCardR({ match }) {
-  let d = new Date(match.date );
+  let d = new Date(match.date);
   let date = d.toDateString().split(" ");
-console.log(match)
+  console.log(match);
   return (
     <>
       <div className={"match-card " + match.status}>
         <div className="card-info">
-          <SportName match={match}/>
+          <SportName match={match} />
           <TeamLogo team={match.team1} />
 
           <div className="match-info">
             <div className="vs">
-            {match.status != "upcoming" ? (
-              <>
-                {match.sport == "cricket" && <CricketScore match={match} />}
-                {match.sport == "hockey" && <HockeyScore match={match} />}
-                {match.sport == "basketball" && <BasketScore match={match} />}
-                {match.sport == "lawn tennis" && <LawnTennisScore match={match} />}
-                {match.sport == "volleyball" && <LawnTennisScore match={match} />}
-                {match.sport == "table tennis" && <LawnTennisScore match={match} />}
-                {match.sport == "badminton" && <LawnTennisScore match={match} />}
-                {match.sport == "squash" && <LawnTennisScore match={match} />}
-                {match.sport == "football" && <HockeyScore match={match} />}
-              </>
-              ) : <div>V/S</div>}
-              </div>
+              {match.status != "upcoming" ? (
+                <>
+                  {match.sport == "cricket" && <CricketScore match={match} />}
+                  {match.sport == "hockey" && <HockeyScore match={match} />}
+                  {match.sport == "basketball" && <BasketScore match={match} />}
+                  {match.sport == "lawn tennis" && (
+                    <LawnTennisScore match={match} />
+                  )}
+                  {match.sport == "volleyball" && (
+                    <LawnTennisScore match={match} />
+                  )}
+                  {match.sport == "table tennis" && (
+                    <LawnTennisScore match={match} />
+                  )}
+                  {match.sport == "badminton" && (
+                    <LawnTennisScore match={match} />
+                  )}
+                  {match.sport == "squash" && <LawnTennisScore match={match} />}
+                  {match.sport == "football" && <HockeyScore match={match} />}
+                </>
+              ) : (
+                <div>V/S</div>
+              )}
+            </div>
 
             <div className="match-time-format">
               {match.status != "live" && (
@@ -74,10 +98,7 @@ console.log(match)
                 {match.status != "completed" ? (
                   <Link
                     target="_blank"
-                    to={
-                      match.locationUrl ||
-                      "https://maps.app.goo.gl/Y438GM3RJka7UE5MA"
-                    }
+                    to={locationVenueMap[match.sport]}
                     className="extra-button location"
                   >
                     <SiGooglemaps
@@ -100,7 +121,7 @@ console.log(match)
                   &nbsp;
                   <span>
                     <HiClock style={{ display: "inline-block" }} /> &nbsp;
-                    {match.time.split(":").splice(0,2).join(":")}&nbsp;
+                    {match.time.split(":").splice(0, 2).join(":")}&nbsp;
                   </span>
                 </>
               )}
