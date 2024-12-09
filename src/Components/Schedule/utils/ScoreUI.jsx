f6f3e3ba1ebcd5615f6571f3f3a105299858c029
Prathamesh Thorat <thoratprathamesh1729@gmail.com>
2024-12-09 10:21:36 +0530

    Over error fixed

--- src/Components/Schedule/utils/ScoreUI.jsx
+++ src/Components/Schedule/utils/ScoreUI.jsx
@@ -20,7 +20,7 @@ export function CricketScore({ match }) {
           <GiAbstract004 style={{ display: "inline-block" }} />
         </div>
         <br />
-        {match.balls / 6 || 0}
+        {match.team1_overs || 0}
         <br />
         <div>
           <GiTennisBall style={{ display: "inline-block" }} />
@@ -39,7 +39,7 @@ export function CricketScore({ match }) {
           <GiAbstract004 style={{ display: "inline-block" }} />
         </div>
         <br />
-        {match.balls / 6 || 0}
+        {match.team2_overs || 0}
         <br />
         <div>
           <GiTennisBall style={{ display: "inline-block" }} />
